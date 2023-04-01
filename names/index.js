2cd6c5152edacb880918af4472697fdea00903f1
ganeshnaik000 <ganeshnaikm000@gmail.com>
2023-04-01 05:50:31 +0000

    changed some features

--- names/index.js
+++ names/index.js
@@ -1,8 +1,8 @@
 const peopleName = require("../country/state/city/index");
 const getFirstName = require("../utilities/utils/index");
 
-const getPeopleInCity = (getFirstName) => {
-  return peopleName(getFirstName);
+const getPeopleInCity = (peopleName) => {
+  return getFirstName(peopleName);
 };
 
 module.exports = getPeopleInCity;
